7d26bc0294bc9217bed8e88ffe26e573dea7d761
WANGZHENGYUN <wangzhengyun@travelsky.com>
2019-10-09 14:53:31 +0800

    delete unuseful thigns

--- poker_rec/src/main/java/com/myopencvdemo/App.java
+++ poker_rec/src/main/java/com/myopencvdemo/App.java
@@ -2,10 +2,6 @@ package com.myopencvdemo;
 
 import android.app.Application;
 
-/**
- * Created by wangzy on 2019/4/26
- * description:
- */
 public class App extends Application {
 
     public static final String tag="opencv";
